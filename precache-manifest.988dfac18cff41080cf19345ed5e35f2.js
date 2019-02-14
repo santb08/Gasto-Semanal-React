498a08738055c024343a8936a65ecd76563d3d39
fxfxrxfx <santi-c-b@outlook.com>
2019-02-14 03:15:33 -0500

    Updates

---
+++ precache-manifest.988dfac18cff41080cf19345ed5e35f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "764ec13685b048aa2ce7",
+    "url": "/gasto-semanal-react/static/js/runtime~main.764ec136.js"
+  },
+  {
+    "revision": "2c0e3cd2b65308f7bf7b",
+    "url": "/gasto-semanal-react/static/js/main.2c0e3cd2.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/gasto-semanal-react/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "2c0e3cd2b65308f7bf7b",
+    "url": "/gasto-semanal-react/static/css/main.b0b0986e.chunk.css"
+  },
+  {
+    "revision": "a468481114517edf54b8bdc4b3df3a6e",
+    "url": "/gasto-semanal-react/index.html"
+  }
+];
